29b46efa3b33b86837db1b42648460b980151aa7
Peter Strøiman <peter@stroiman.com>
2015-05-07 16:46:07 +0200

    Added documentation

--- automapper.go
+++ automapper.go
@@ -1,11 +1,31 @@
 // Package automapper provides support for mapping between two different types
-// with compatible fields
+// with compatible fields. The intended application for this is when you use
+// one set of types to represent DTOs (data transfer objects, e.g. json data),
+// and a different set of types internally in the application. Using this
+// package can help converting from one type to another.
 package automapper
 
 import (
 	"reflect"
 )
 
+// Map fills out the fields in dest with values from source. All fields in the
+// destination object must exist in the source object.
+//
+// Object hierarchies with nested structs and slices are supported, as long as
+// type types of nested structs/slices follow the same rules, i.e. all fields
+// in destination structs must be found on the source struct.
+//
+// Embedded/anonymous structs are supported
+//
+// Values that are not exported/not public will not be mapped.
+//
+// It is a design decision to panic when a field cannot be mapped in the
+// destination to ensure that a renamed field in either the source or
+// destination does not result in subtle silent buge.
+//
+// BUG(ps) - It is the intend that the code should panic early when mapping
+// incompatible types. Empty slices are not supported however
 func Map(source, dest interface{}) {
 	var destType = reflect.TypeOf(dest)
 	if destType.Kind() != reflect.Ptr {
